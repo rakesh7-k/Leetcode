980fdd3bbb10f53147efb911f239dbc0184f313d
Rakesh k <rakesh.k07062006@gmail.com>
2025-12-08 17:25:35 +0530

    Time: 0 ms (100%), Space: 18 MB (12.8%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        max_wealth = 0
+        for customer in accounts:
+            max_wealth = max(max_wealth, sum(customer))
+        return max_wealth
