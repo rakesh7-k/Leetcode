272a7d7ef9ad9af1413b42d855d17917e7c2ac48
Rakesh k <rakesh.k07062006@gmail.com>
2026-06-16 17:47:48 +0530

    Time: 4 ms (76.97%), Space: 23.2 MB (82.24%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.py
@@ -1,15 +1,19 @@
+
 class Solution:
     def processStr(self, s: str) -> str:
-        res=[]
-        for i in s:
-            if i.isalpha():
-                res.append(i)
-            elif i=='*':
-                if len(res)!=0:
-                    res.pop(-1)
-            elif i=='#':
-                res=res*2
-            elif i=='%':
-                res=res[::-1]
-        return ''.join(res)
-        
+        ans = ""
+
+        for x in s:
+            if x == '*':
+                ans = ans[:-1]
+
+            elif x == '#':
+                ans += ans
+
+            elif x == '%':
+                ans = ans[::-1]
+
+            else:
+                ans +=x
+
+        return ans
